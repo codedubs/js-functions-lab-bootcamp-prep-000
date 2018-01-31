99bc3d453459f7db0e5a9b8396cd193f3b5958b1
Pitta <mailez4all@gmail.com>
2018-01-31 07:18:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(girak) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidaysTo(Christmas, girak) {
+  return `Happy ${holiday}, ${name}`
+}
+
+function 
